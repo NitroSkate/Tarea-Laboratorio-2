1a7dde3b816071c2e4f1befc50898d83c262949c
NitroSkate <00159817@uca.edu.sv>
2020-03-30 20:50:26 -0600

    Terminado

---
+++ src/main/java/com/uca/capas/controller/MainController.java
@@ -0,0 +1,24 @@
+package com.uca.capas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.uca.capas.domain.Usuario;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/Login")
+	public String enviarForm(Usuario usuario) {
+		return "Login";
+	}
+	@PostMapping("/Login")
+	public String procesarForm(Usuario usuario) {
+		if(usuario.getNombre().equals("admin") && usuario.getPassword().equals("password")) {
+			return "MostrarMensajeV";
+		}
+		return "MostrarMensajeF";
+	}
+
+}
